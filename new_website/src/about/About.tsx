4f9ebdd62b73c105a7889b7b55c94690ebb0fdc8
Tomas Godoybaca <tommygodoybaca@outlook.com>
2025-09-08 15:52:57 -0600

    added temporary pages with placeholder content

---
+++ new_website/src/about/About.tsx
@@ -0,0 +1,8 @@
+function About() {
+
+    return (
+        <p>This is the About Page</p>
+    )
+}
+
+export default About
---
+++ new_website/src/destiny/Destiny.tsx
@@ -0,0 +1,8 @@
+function Destiny() {
+
+    return (
+        <p>This is the Destiny Page</p>
+    )
+}
+
+export default Destiny
