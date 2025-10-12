89be0c2406365295adb6daed2aa97dd004de7111
Tomas Godoybaca <tommygodoybaca@outlook.com>
2025-10-12 14:13:18 -0600

    main page changes - removed TypingReveal to avoid potential performance issues - styling changes to the image display - the image grid now looks like a collection of polaroids - single image view now has the arrows and exit in correct positions - many styling changes for the image display and grid

--- new_website/src/Home.tsx
+++ new_website/src/Home.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import TypingReveal from './components/TypingReveal.tsx'
 import NavBar from './components/NavBar.tsx'
 
 interface Images {
@@ -15,19 +14,23 @@ function ImageDisplay({ images }: { images: object[] }) {
   //Just an image display, maybe displays a group of images so that the user can use arrows to change which one is visible
   //on click the image will scale up to fill the screen
   //images will be displayed on a grid that will fill the parent
-  //
+  //transform: "rotate({Math.floor(Math.random() * (8 - (-8)) + (-8)))"
 
   //image has "any" type because of conflict with declaring the object types
   const imageObject = images.map((image: any, index: number) => {
-    return <img src={image.path} alt={image.alt} key={index} onClick={() => { setImageIndex(index); setImageFocused(true); }} />
+    //picks a random number between -3 and 3 to add some variation to the polaroids
+    let randomRotate = Math.floor(Math.random() * (3 - (-3)) + (-3));
+    return <div className="polaroid" style={{ transform: `rotate(${randomRotate}deg)` }}>
+      <img src={image.path} alt={image.alt} key={index} onClick={() => { setImageIndex(index); setImageFocused(true); }} />
+    </div>
   })
 
   if (imageFocused) {
-    return <div className='singleImage'>
-      <img src={images[imageIndex].path} alt={images[imageIndex].alt}></img>
-      <img src="src/assets/close_24dp_C1E0DC_FILL0_wght400_GRAD-25_opsz24.svg" alt="Exit Button" onClick={() => setImageFocused(false)} />
-      <img src="src/assets/keyboard_arrow_left_24dp_C1E0DC_FILL0_wght400_GRAD-25_opsz24.svg" alt="Left Button" onClick={() => setImageIndex(imageIndex - 1)} />
-      <img src="src/assets/keyboard_arrow_right_24dp_C1E0DC_FILL0_wght400_GRAD-25_opsz24.svg" alt="Right Button" onClick={() => setImageIndex(imageIndex + 1)} />
+    return <div className='singleImageDiv'>
+      <img src={images[imageIndex].path} alt={images[imageIndex].alt} className="singleImage"></img>
+      <img src="src/assets/close_24dp_C1E0DC_FILL0_wght400_GRAD-25_opsz24.svg" alt="Exit Button" onClick={() => setImageFocused(false)} className="imageExit imageButton" />
+      {(imageIndex === 0) ? null : <img src="src/assets/keyboard_arrow_left_24dp_C1E0DC_FILL0_wght400_GRAD-25_opsz24.svg" alt="Left Button" onClick={() => setImageIndex(imageIndex - 1)} className="imageLeft imageButton" />}
+      {(imageIndex === images.length - 1) ? null : <img src="src/assets/keyboard_arrow_right_24dp_C1E0DC_FILL0_wght400_GRAD-25_opsz24.svg" alt="Right Button" onClick={() => setImageIndex(imageIndex + 1)} className="imageRight imageButton" />}
     </div>
 
   } else {
@@ -36,8 +39,6 @@ function ImageDisplay({ images }: { images: object[] }) {
 }
 
 function Home() {
-  const [newText, setNewText] = useState("Hello There")
-  const [textChanged, setTextChanged] = useState(false)
 
   const images: Images[] = [
     {
@@ -66,8 +67,9 @@ function Home() {
     <>
       <NavBar />
       <main>
-        <h1><TypingReveal text={newText} delay={100} textChanged={textChanged} setTextChanged={setTextChanged} /></h1>
-        <input
+        <h1>My name is Tomas Godoybaca</h1>
+        <p>I am a front-end developer / designer based in Saskatoon, Saskatchewan.</p>
+        {/* <input
           name="textInput"
           type="text"
           value={newText}
@@ -78,7 +80,7 @@ function Home() {
             setTextChanged(true);
 
           }}
-        />
+        /> */}
         <ImageDisplay images={images} />
       </main>
     </>
